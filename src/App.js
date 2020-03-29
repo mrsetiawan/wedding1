1425585a57f229bd252216fa62caa1c7ee30c01d
rahmat <muhamadrahmatsetiawan@gmail.com>
2020-03-29 10:10:22 +0700

    update component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react'
-import { Link as Link, Element, animateScroll as scroll } from 'react-scroll'
-import MyNavbar from './components/ui/MyNavbar'
+import { Link as Target, Element, animateScroll as scroll } from 'react-scroll'
 import image1 from '../src/components/assets/slide1.jpg'
+import image2 from '../src/components/assets/slide2.jpg'
+import image3 from '../src/components/assets/slide3.jpg'
+import image4 from '../src/components/assets/slide4.jpg'
 import first from '../src/components/assets/first.png'
 import middle from '../src/components/assets/middle.png'
 import last from '../src/components/assets/last.png'
 import {
-  Carousel,
   Container,
+  Carousel,
   Col,
   Row,
   Navbar
@@ -17,45 +19,63 @@ import './App.css';
 
 export default class App extends Component {
 
+  constructor(props) {
+    super(props)
+    this.state = {
+      data: []
+    }
+  }
+
   scrollBottom = () => scroll.scrollToBottom()
 
   render() {
     return (
       <div>
-        <Navbar sticky="bottom">
-          <ul>
-            <li><Link activeClass="active" to="firstElement" spy={true} smooth={true} offset={50}></Link><p onClick={this.scrollBottom}>tes 1</p></li>
-            <li>tes 1</li>
-            <li>tes 1</li>
-            <li>tes 1</li>
-          </ul>
+        <Navbar bg="light" variant="light" className='sticky-top'>
+          <Container>
+            <Col>
+              <ul className='nav-list'>
+                <li className='nav-item'><Target activeClass="active" to="test1" spy={true} smooth={true} offset={0} duration={600} >CEREMONY</Target></li>
+                <li className='nav-item'><Target activeClass="active" to="test2" spy={true} smooth={true} offset={0} duration={600} >RECEPTION</Target></li>
+                <li className='nav-item'><Target activeClass="active" to="test3" spy={true} smooth={true} offset={0} duration={600} >AFTERPARTY</Target></li>
+                <li className='nav-item'><Target activeClass="active" to="brand" spy={true} smooth={true} offset={0} duration={600} ><span className='brand'><b>Rahmat & Indah</b></span></Target></li>
+                <li className='nav-item'><Target activeClass="active" to="test3" spy={true} smooth={true} offset={0} duration={600} >ACCOMODATION</Target></li>
+                <li className='nav-item'><Target activeClass="active" to="test3" spy={true} smooth={true} offset={0} duration={600} >REGISTRY</Target></li>
+                <li className='nav-item'><Target activeClass="active" to="test3" spy={true} smooth={true} offset={0} duration={600} >RSVP</Target></li>
+              </ul>
+            </Col>
+          </Container>
         </Navbar>
-        <Carousel>
-          <Carousel.Item>
-            <img
-              className="banner-image"
-              src={image1}
-              alt="First slide"
-            />
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className="banner-image"
-              src={image1}
-              alt="Third slide"
-            />
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className="banner-image"
-              src={image1}
-              alt="Third slide"
-            />
-          </Carousel.Item>
-        </Carousel>
+        <Element name='brand' style={{ width: '100%' }}>
+          <Carousel>
+            <Carousel.Item>
+              <div className='layer'>
+                <img src={image1} alt='1'/>
+              </div>
+              <Carousel.Caption>
+                <h3>We're engaged!</h3>
+                <p>Insyaallah November 2020 Nikah</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img src={image2} alt='2'/>
+              <Carousel.Caption>
+                <h3>We're engaged!</h3>
+                <p>Insyaallah November 2020 Nikah</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img src={image3} alt='3'/>
+              <Carousel.Caption>
+                <h3>We're engaged!</h3>
+                <p>Insyaallah November 2020 Nikah</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+          </Carousel>
+        </Element>
         <Row>
-          <Element name='firstElement'>
-            <Col xs={12} className='element-child text-center'>
+          <Element name='test1' style={{ width: '100%' }}>
+            <Col xs={12} className='element-child'>
               <img src={first} alt="first img" />
               <h3><b>The Ceremony</b></h3>
               <p className='text-secondary'>4:00 PM</p>
@@ -64,26 +84,32 @@ export default class App extends Component {
               <p>Flagstaff, AZ 86001</p>
             </Col>
           </Element>
-          <Col xs={12} className='element-child text-center' style={{ backgroundColor: '#E9967A' }}>
-            <img src={middle} alt="first img" />
-            <h3><b>The Ceremony</b></h3>
-            <p className='text-secondary'>4:00 PM</p>
-            <p> Grace Church </p>
-            <p> 37 Bear Blvd</p>
-            <p>Flagstaff, AZ 86001</p>
-          </Col>
-          <Col xs={12} className='element-child text-center'>
-            <img src={last} alt="first img" />
-            <h3><b>The Ceremony</b></h3>
-            <p className='text-secondary'>4:00 PM</p>
-            <p> Grace Church </p>
-            <p> 37 Bear Blvd</p>
-            <p>Flagstaff, AZ 86001</p>
-          </Col>
-          <Col xs={12} className='element-child text-center' style={{ backgroundColor: 'rgb(133, 183, 180 )' }}>
-            Footer
-          </Col>
+
+          <Element name='test2' style={{ width: '100%' }}>
+            <Col xs={12} className='element-child' style={{ backgroundColor: '#E9967A' }}>
+              <img src={middle} alt="first img" />
+              <h3><b>The Ceremony</b></h3>
+              <p className='text-secondary'>4:00 PM</p>
+              <p> Grace Church </p>
+              <p> 37 Bear Blvd</p>
+              <p>Flagstaff, AZ 86001</p>
+            </Col>
+          </Element>
+
+          <Element name='test3' style={{ width: '100%' }}>
+            <Col xs={12} className='element-child'>
+              <img src={last} alt="first img" />
+              <h3><b>The Ceremony</b></h3>
+              <p className='text-secondary'>4:00 PM</p>
+              <p> Grace Church </p>
+              <p> 37 Bear Blvd</p>
+              <p>Flagstaff, AZ 86001</p>
+            </Col>
+          </Element>
         </Row>
+        <Col xs={12} className='element-child text-center' style={{ backgroundColor: 'rgb(133, 183, 180 )' }}>
+          template by @muhamadrahmatsetiawan
+          </Col>
       </div>
     )
   }
